28b07a8bdbfaa4671c62c363c81d0e6fdb1c77bb
yassataiseer <56459253+yassataiseer@users.noreply.github.com>
2020-02-21 00:59:33 -0500

    Add files via upload

--- startup/app.py
+++ startup/app.py
@@ -11,19 +11,37 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    print('1')
     return render_template("home.html")
 
 
 @app.route("/story", methods=['GET','POST'])
 def other():
+    print('2')
     return render_template("story.html")
 
 
 @app.route("/home", methods=['GET','POST'])
 def another():
+    print('3')
     return render_template("home.html")
 
+@app.route("/yourstories", methods=['GET','POST'])
+def anotherone():
+    print('3')
+    return render_template("storypage.html")
 
+
+@app.route("/", methods=['POST'])
+def values():
+    print('4')
+    title= request.form['title']
+    genres= request.form['genres']
+    date = request.form['birthday']
+    story = request.form['story']
+    name = request.form['name']
+
+    return render_template("home.html",title=title,genres=genres,date=date,story=story,name=name)
 
 
 
