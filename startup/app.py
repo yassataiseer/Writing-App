c0c9230a114b098bb01fca96d258d84f211572e7
yassataiseer <56459253+yassataiseer@users.noreply.github.com>
2020-03-06 18:48:32 -0500

    Add files via upload

--- startup/app.py
+++ startup/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template,request,session, url_for
-
-
 import json
 import urllib.request
-
-
+import sqlite3 
+import csv
+import openpyxl
+import pandas as pd 
+conn = sqlite3.connect('data1.db', check_same_thread=False) 
+c = conn.cursor() 
+c.execute('CREATE TABLE IF NOT EXISTS RecordONE (Username TEXT , Password TEXT)') 
 app = Flask(__name__)
-
 
 
 @app.route("/")
@@ -26,13 +28,26 @@ def another():
     print('3')
     return render_template("home.html")
 
+
+
 @app.route("/yourstories", methods=['GET','POST'])
 def anotherone():
-    print('3')
+    print('4')
     return render_template("storypage.html")
 
+@app.route("/login", methods=['GET','POST'])
+def login():
+    print('5')
+    return render_template("login.html")
 
-@app.route("/", methods=['POST'])
+
+
+@app.route("/signup", methods=['GET','POST'])
+def signup():
+    print('6')
+    return render_template("signup.html")
+
+@app.route("/story20", methods=['POST'])
 def values():
     print('4')
     title= request.form['title']
@@ -40,12 +55,43 @@ def values():
     date = request.form['birthday']
     story = request.form['story']
     name = request.form['name']
+    with  open("story.csv","a")as file:
+        writer = csv.writer(file)
+        writer.writerow([title , genres, date, story,name])       
+        print(file)
+        file.close()
+    items = title,genres,date,story,name
 
-    return render_template("home.html",title=title,genres=genres,date=date,story=story,name=name)
+    print(items[0])
+    return render_template("home.html",items=items)
+
+@app.route("/sign", methods=['POST'])
+def sign():
+    username = request.form['username']
+    password = request.form['pascode']
+    c.execute("INSERT INTO RecordONE (Username, Password) VALUES(?, ?)", (username, password)) 
+    c.execute("SELECT * FROM RecordONE ")
+    rows = c.fetchall()
+    for row in rows:
+        if row[0]==username:
+            return 'this Userame is already used please try anotherone'
+        else:
+            conn.commit() 
+            return render_template("home.html", username = username, password=password)
 
 
-
-
+@app.route("/login101", methods=['POST'])
+def login101():
+    username = request.form['username1']
+    password = request.form['pascode1']
+    print(username)
+    c.execute("SELECT * FROM RecordONE ")
+    rows = c.fetchall()
+    for row in rows:
+        if row[0]==username and row[1] ==password:
+            return render_template("home.html")
+    
+    return "Invalid creds"
 
 if __name__ == '__main__':
     app.run(debug=True)
